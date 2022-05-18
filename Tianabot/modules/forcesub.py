731eb3c08fd2082b70ac7e9b0dc07204ee224cc9
Mhd Althaf <93602145+Muhammedalthaf174@users.noreply.github.com>
2022-05-18 15:32:06 +0530

    Update forcesub.py

--- Tianabot/modules/forcesub.py
+++ Tianabot/modules/forcesub.py
@@ -198,6 +198,5 @@ __help__ = """
   💡If you disable fsub, you need to set again for working.. /fsub {channel username} 
  ❍ /fsub clear - To unmute all members who muted by me.
 """
-
 __mod_name__ = "ғ-sᴜʙ/ғᴇᴅs"
 
